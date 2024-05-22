4becce1e9c806ffb39ef53b70badacccf063a4e8
Rimas74 <gintilas.rimantas@gmail.com>
2024-05-22 14:06:50 +0300

    completed NoteServiceUnitTest and CategoryServiceUnitTest

--- UnitTestsBusinessLogic/CategoryServiceUnitTest.cs
+++ UnitTestsBusinessLogic/CategoryServiceUnitTest.cs
@@ -1,5 +1,5 @@
 using AutoMapper;
-using Castle.Core.Logging;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Notepad.BusinessLogic;
@@ -7,29 +7,29 @@ using Notepad.Common.DTOs;
 using Notepad.DataAccess;
 using Notepad.Repositories;
 using Notepad.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace UnitTestsBusinessLogic
 {
     public class CategoryServiceUnitTest : IAsyncLifetime
     {
+        private NotepadContext _context;
         private CategoryService _categoryService;
 
-        private readonly Mock<ICategoryRepository> _categoryRepositoryMock = new Mock<ICategoryRepository>();
         private readonly Mock<IMapper> _mapperMock = new Mock<IMapper>();
         private readonly Mock<ILogger<CategoryService>> _loggerMock = new Mock<ILogger<CategoryService>>();
 
-        private List<Category> _categories;
         private List<CategoryDTO> _categoryDTOs;
-        private UpdateCategoryDTO _updateCategoryDto;
+        private UpdateCategoryDTO _updateCategoryDTO;
+        private CreateCategoryDTO _createCategoryDTO;
 
-
-        public Task InitializeAsync()
+        public async Task InitializeAsync()
         {
-            _categories = new List<Category>
-            {
-                new Category {CategoryId=1, Name="Category1", UserId="user1"},
-                new Category {CategoryId=2, Name="Category2", UserId="user2"},
-            };
+            _context = await GetDatabaseContext();
 
             _categoryDTOs = new List<CategoryDTO>
             {
@@ -37,78 +37,74 @@ namespace UnitTestsBusinessLogic
                 new CategoryDTO { CategoryId = 2, Name = "Category2", UserId = "user2" }
             };
 
-            _updateCategoryDto = new UpdateCategoryDTO
+            _updateCategoryDTO = new UpdateCategoryDTO
             {
                 Name = "Updated Category"
             };
 
+            _createCategoryDTO = new CreateCategoryDTO
+            {
+                Name = "New Category"
+            };
 
-            _categoryRepositoryMock.Setup(repo => repo.GetAllAsync()).ReturnsAsync(_categories);
-            _categoryRepositoryMock.Setup(repo => repo.GetAsyncById(It.IsAny<int>())).ReturnsAsync((int id) => _categories.Find(c => c.CategoryId == id));
-            _categoryRepositoryMock.Setup(repo => repo.GetCategoriesByUserIdAsync(It.IsAny<string>())).ReturnsAsync((string userId) => _categories.FindAll(c => c.UserId == userId));
-            _categoryRepositoryMock.Setup(repo => repo.GetCategoryByNameAndUserIdAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync((string name, string userId) => _categories.Find(c => c.Name == name && c.UserId == userId));
-            _categoryRepositoryMock.Setup(repo => repo.UpdateAsync(It.IsAny<Category>())).Returns(Task.CompletedTask);
-
+            var categoryRepositoryMock = new CategoryRepository(_context);
 
             _mapperMock.Setup(m => m.Map<IEnumerable<CategoryDTO>>(It.IsAny<IEnumerable<Category>>())).Returns((IEnumerable<Category> source) =>
             {
                 return source.Select(s => new CategoryDTO { CategoryId = s.CategoryId, Name = s.Name, UserId = s.UserId });
             });
-            _mapperMock.Setup(m => m.Map<CategoryDTO>(It.IsAny<Category>())).Returns((Category category) => category != null ? _categoryDTOs.Find(c => c.CategoryId == category.CategoryId) ?? new CategoryDTO() : null);
 
-            _mapperMock.Setup(m => m.Map(It.IsAny<UpdateCategoryDTO>(), It.IsAny<Category>())).Callback<UpdateCategoryDTO, Category>((dto, category) => category.Name = dto.Name);
+            _mapperMock.Setup(m => m.Map<CategoryDTO>(It.IsAny<Category>())).Returns((Category category) =>
+                category != null ? new CategoryDTO { CategoryId = category.CategoryId, Name = category.Name, UserId = category.UserId } : null);
 
+            _mapperMock.Setup(m => m.Map<Category>(It.IsAny<CreateCategoryDTO>())).Returns((CreateCategoryDTO dto) =>
+                new Category { Name = dto.Name, UserId = "user3" });
 
+            _mapperMock.Setup(m => m.Map(It.IsAny<UpdateCategoryDTO>(), It.IsAny<Category>())).Callback<UpdateCategoryDTO, Category>((dto, category) =>
+            {
+                category.Name = dto.Name;
+            });
 
-            _categoryService = new CategoryService(_categoryRepositoryMock.Object, _mapperMock.Object, _loggerMock.Object);
+            _categoryService = new CategoryService(categoryRepositoryMock, _mapperMock.Object, _loggerMock.Object);
+        }
 
-            return Task.CompletedTask;
+        private async Task<NotepadContext> GetDatabaseContext()
+        {
+            var options = new DbContextOptionsBuilder<NotepadContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new NotepadContext(options);
+            context.Database.EnsureCreated();
+
+            if (!context.Categories.Any())
+            {
+                context.Categories.AddRange
+                (
+                    new Category { CategoryId = 1, Name = "Category1", UserId = "user1" },
+                    new Category { CategoryId = 2, Name = "Category2", UserId = "user2" }
+                );
+                await context.SaveChangesAsync();
+            }
+            return context;
         }
 
         public Task DisposeAsync()
         {
+            _context?.Dispose();
             return Task.CompletedTask;
         }
 
         [Fact]
-
-        public async Task GetAllCategoriesAsync_ShouldreturnAllcategories()
+        public async Task GetAllCategoriesAsync_ShouldReturnAllCategories()
         {
             //Act
-
             var result = await _categoryService.GetAllCategoriesAsync();
 
             //Assert
-
             Assert.NotNull(result);
             Assert.Equal(_categoryDTOs.Count, result.Count());
             Assert.Equal(_categoryDTOs.Select(c => c.Name), result.Select(c => c.Name));
-
-        }
-
-        [Fact]
-        public async Task CreateCategoryAsync_ShouldCreateCategory_WhenCategoryDoNotExist()
-        {
-            //Arrange
-
-            var createCategoryDto = new CreateCategoryDTO { Name = "New Category" };
-            var userId = "user3";
-            var category = new Category { CategoryId = 3, Name = "New Category", UserId = userId };
-
-            _mapperMock.Setup(m => m.Map<Category>(It.IsAny<CreateCategoryDTO>())).Returns(category);
-            _categoryRepositoryMock.Setup(repo => repo.AddAsync(It.IsAny<Category>())).Returns(Task.CompletedTask);
-            _mapperMock.Setup(m => m.Map<CategoryDTO>(It.IsAny<Category>())).Returns(new CategoryDTO { CategoryId = 3, Name = "New Category", UserId = userId });
-
-            //Act
-
-            var result = await _categoryService.CreateCategoryAsync(createCategoryDto, userId);
-
-            //Assert
-
-            Assert.NotNull(result);
-            Assert.Equal(category.Name, result.Name);
-            _categoryRepositoryMock.Verify(repo => repo.AddAsync(It.Is<Category>(c => c.Name == createCategoryDto.Name && c.UserId == userId)), Times.Once);
-
         }
 
         [Fact]
@@ -121,7 +117,6 @@ namespace UnitTestsBusinessLogic
             var result = await _categoryService.GetCategoryByIdAsync(categoryId);
 
             //Assert
-
             Assert.NotNull(result);
             Assert.Equal(categoryId, result.CategoryId);
         }
@@ -136,40 +131,52 @@ namespace UnitTestsBusinessLogic
             var result = await _categoryService.GetCategoryByIdAsync(categoryId);
 
             //Assert
-
             Assert.Null(result);
         }
+
+        [Fact]
+        public async Task CreateCategoryAsync_ShouldCreateCategory_WhenCategoryDoesNotExist()
+        {
+            //Arrange
+            var userId = "user3";
+
+            //Act
+            var result = await _categoryService.CreateCategoryAsync(_createCategoryDTO, userId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(_createCategoryDTO.Name, result.Name);
+            Assert.Equal(userId, result.UserId);
+        }
+
         [Fact]
         public async Task UpdateCategoryAsync_ShouldUpdateCategory_WhenCategoryExists()
         {
-            // Arrange
-            var userId = "user1";
-            var categoryDTOs = _categoryDTOs.FindAll(c => c.UserId == userId);
+            //Arrange
+            var categoryId = 1;
 
-            // Act
-            var result = await _categoryService.GetCategoriesByUserIdAsync(userId);
+            //Act
+            await _categoryService.UpdateCategoryAsync(categoryId, _updateCategoryDTO, "user1");
 
-            // Assert
-            Assert.NotNull(result);
-            Assert.Equal(categoryDTOs.Count, result.Count());
-            Assert.Equal(categoryDTOs.Select(c => c.Name), result.Select(c => c.Name));
+            //Assert
+            var updatedCategory = await _context.Categories.FindAsync(categoryId);
+            Assert.Equal(_updateCategoryDTO.Name, updatedCategory.Name);
         }
+
         [Fact]
         public async Task DeleteCategoryAsync_ShouldDeleteCategory_WhenCategoryExists()
         {
             //Arrange
-
             var categoryId = 1;
-            var category = _categories.Find(c => c.CategoryId == categoryId);
-            _categoryRepositoryMock.Setup(repo => repo.DeleteAsync(It.IsAny<Category>())).Returns(Task.CompletedTask);
 
             //Act
             await _categoryService.DeleteCategoryAsync(categoryId);
 
             //Assert
-
-            _categoryRepositoryMock.Verify(repo => repo.DeleteAsync(It.Is<Category>(c => c.CategoryId == categoryId)), Times.Once);
+            var deletedCategory = await _context.Categories.FindAsync(categoryId);
+            Assert.Null(deletedCategory);
         }
+
         [Fact]
         public async Task GetCategoriesByUserIdAsync_ShouldReturnCategories_WhenUserHasCategories()
         {
@@ -178,11 +185,9 @@ namespace UnitTestsBusinessLogic
             var categoryDTOs = _categoryDTOs.FindAll(c => c.UserId == userId);
 
             //Act
-
             var result = await _categoryService.GetCategoriesByUserIdAsync(userId);
 
             //Assert
-
             Assert.NotNull(result);
             Assert.Equal(categoryDTOs.Count, result.Count());
             Assert.Equal(categoryDTOs.Select(c => c.Name), result.Select(c => c.Name));
--- UnitTestsBusinessLogic/NoteServiceUnitTest.cs
+++ UnitTestsBusinessLogic/NoteServiceUnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -9,9 +10,10 @@ using Notepad.Repositories;
 using Notepad.Repositories.Entities;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace UnitTestsBusinessLogic
 {
@@ -27,18 +29,66 @@ namespace UnitTestsBusinessLogic
         private List<NoteDTO> _noteDTOs;
         private NoteUpdateDTO _noteUpdateDTO;
         private CreateNoteDTO _createNoteDTO;
+        private NoteUpdateImageDTO _noteUpdateImageDTO;
 
         public async Task InitializeAsync()
         {
             _context = await GetDatabaseContext();
 
+            _noteDTOs = new List<NoteDTO> {
+                new NoteDTO { NoteId = 1, Title = "Note1", Content = "Content1", CategoryId = 1, UserId = "user1" },
+                new NoteDTO { NoteId = 2, Title = "Note2", Content = "Content2", CategoryId = 1, UserId = "user1" }
+            };
 
+            _noteUpdateDTO = new NoteUpdateDTO
+            {
+                Title = "Updated Note",
+                Content = "Updated Content",
+                CategoryId = 1
+            };
+
+            _createNoteDTO = new CreateNoteDTO
+            {
+                Title = "New Note",
+                Content = "New Content",
+                CategoryId = 1,
+                Image = null
+            };
+
+            _noteUpdateImageDTO = new NoteUpdateImageDTO
+            {
+                Image = new FormFile(new MemoryStream(), 0, 0, null, "test.jpg")
+            };
+
+            var noteRepositoryMock = new NoteRepository(_context);
+
+            _mapperMock.Setup(m => m.Map<IEnumerable<NoteDTO>>(It.IsAny<IEnumerable<Note>>())).Returns((IEnumerable<Note> source) =>
+            {
+                return source.Select(s => new NoteDTO { NoteId = s.NoteId, Title = s.Title, Content = s.Content, UserId = s.UserId, CategoryId = s.CategoryId });
+            });
+
+            _mapperMock.Setup(m => m.Map<NoteDTO>(It.IsAny<Note>())).Returns((Note note) =>
+                note != null ? new NoteDTO { NoteId = note.NoteId, Title = note.Title, Content = note.Content, UserId = note.UserId, CategoryId = note.CategoryId } : null);
+
+            _mapperMock.Setup(m => m.Map<Note>(It.IsAny<CreateNoteDTO>())).Returns((CreateNoteDTO dto) =>
+                new Note { Title = dto.Title, Content = dto.Content, CategoryId = dto.CategoryId, UserId = "user3", ImagePath = "default/path.jpg" });
+
+            _mapperMock.Setup(m => m.Map(It.IsAny<NoteUpdateDTO>(), It.IsAny<Note>())).Callback<NoteUpdateDTO, Note>((dto, note) =>
+            {
+                note.Title = dto.Title;
+                note.Content = dto.Content;
+                note.CategoryId = dto.CategoryId.Value;
+            });
+
+            _fileManagerMock.Setup(fm => fm.SaveImageAsync(It.IsAny<IFormFile>())).ReturnsAsync("path/to/image.jpg");
+
+            _noteService = new NoteService(noteRepositoryMock, _fileManagerMock.Object, _mapperMock.Object, _loggerMock.Object);
         }
 
         private async Task<NotepadContext> GetDatabaseContext()
         {
             var options = new DbContextOptionsBuilder<NotepadContext>()
-                .UseInMemoryDatabase(databaseName: System.Guid.NewGuid().ToString())
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
                 .Options;
 
             var context = new NotepadContext(options);
@@ -47,12 +97,13 @@ namespace UnitTestsBusinessLogic
             if (!context.Notes.Any())
             {
                 context.Notes.AddRange
-                    (
+                (
                     new Note { NoteId = 1, Title = "Note1", Content = "Content1", ImagePath = "path1.jpg", CategoryId = 1, UserId = "user1" },
-                    new Note { NoteId = 2, Title = "Note2", Content = "Content2", ImagePath = "path2.jpg", CategoryId = 2, UserId = "user2" }
-                    );
+                    new Note { NoteId = 2, Title = "Note2", Content = "Content2", ImagePath = "path2.jpg", CategoryId = 1, UserId = "user1" }
+                );
                 await context.SaveChangesAsync();
             }
+            return context;
         }
 
         public Task DisposeAsync()
@@ -61,6 +112,117 @@ namespace UnitTestsBusinessLogic
             return Task.CompletedTask;
         }
 
+        [Fact]
+        public async Task GetAllNotesAsync_ShouldReturnAllNotes()
+        {
+            //Act
+            var result = await _noteService.GetAllNotesAsync();
 
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(_noteDTOs.Count, result.Count());
+            Assert.Equal(_noteDTOs.Select(n => n.Title), result.Select(n => n.Title));
+        }
+
+        [Fact]
+        public async Task GetNotesByUserIdAsync_ShouldReturnNotes_WhenUserHasNotes()
+        {
+            //Arrange
+            var userId = "user1";
+            var notesDTOs = _noteDTOs.FindAll(n => n.UserId == userId);
+
+            //Act
+            var result = await _noteService.GetNotesByUserIdAsync(userId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(notesDTOs.Count, result.Count());
+            Assert.Equal(notesDTOs.Select(n => n.Title), result.Select(n => n.Title));
+        }
+
+        [Fact]
+        public async Task GetNoteByIdAsync_ShouldReturnNote_WhenNoteExists()
+        {
+            //Arrange
+            var noteId = 1;
+
+            //Act
+            var result = await _noteService.GetNoteByIdAsync(noteId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(noteId, result.NoteId);
+        }
+
+        [Fact]
+        public async Task GetNoteByIdAsync_ShouldReturnNull_WhenNoteDoesNotExists()
+        {
+            //Arrange
+            var noteId = 3;
+
+            //Act
+            var result = await _noteService.GetNoteByIdAsync(noteId);
+
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task CreateNoteAsync_ShouldCreateNote()
+        {
+            //Arrange
+            var userId = "user3";
+
+            //Act
+            var result = await _noteService.CreateNoteAsync(_createNoteDTO, userId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(_createNoteDTO.Title, result.Title);
+            Assert.Equal(userId, result.UserId);
+        }
+
+        [Fact]
+        public async Task UpdateNoteDetailsAsync_ShouldUpdateNote_WhenNoteExists()
+        {
+            //Arrange
+            var noteId = 1;
+
+            //Act
+            await _noteService.UpdateNoteDetailsAsync(noteId, _noteUpdateDTO);
+
+            //Assert
+            var updatedNote = await _context.Notes.FindAsync(noteId);
+            Assert.Equal(_noteUpdateDTO.Title, updatedNote.Title);
+            Assert.Equal(_noteUpdateDTO.Content, updatedNote.Content);
+        }
+
+        [Fact]
+        public async Task UpdateNoteImageAsync_ShouldUpdateNote_WhenNoteExists()
+        {
+            //Arrange
+            var noteId = 1;
+
+            //Act
+            await _noteService.UpdateNoteImageAsync(noteId, _noteUpdateImageDTO);
+
+            //Assert
+            var updatedNote = await _context.Notes.FindAsync(noteId);
+            Assert.Equal("path/to/image.jpg", updatedNote.ImagePath);
+        }
+
+        [Fact]
+        public async Task DeleteNoteAsync_ShouldDeleteNote_WhenNoteExists()
+        {
+            //Arrange
+            var noteId = 1;
+
+            //Act
+            await _noteService.DeleteNoteAsync(noteId);
+
+            //Assert
+            var deletedNote = await _context.Notes.FindAsync(noteId);
+            Assert.Null(deletedNote);
+        }
     }
 }
